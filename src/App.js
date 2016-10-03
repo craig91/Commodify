dbcd5308f65b0a6a22c5baa257cdf9886324fb59
Rabiya Watson-Maiorana <ormabe@nyc.rr.com>
2016-10-03 12:11:52 -0400

    navbar & main component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link, hashHistory} from 'react-router'
+import $ from 'jquery';
+import NavBar from './navigation'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
-var HelloWorld = React.createClass({
-  render: function() {
+
+var App = React.createClass({
+  render: function () {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <NavBar />
+        {this.props.children}
       </div>
     )
   }
 })
 
+var Air = React.createClass ({
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+})
+
+var Honey = React.createClass ({
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+})
+
+var Land = React.createClass ({
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+})
+
+var Seeds = React.createClass ({
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+})
+
+var Water = React.createClass ({
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('root')
-);
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    <Route path="air" component={Air}></Route>
+    <Route path="honey" component={Honey}></Route>
+    <Route path="land" component={Land}></Route>
+    <Route path="seeds" component={Seeds}></Route>
+    <Route path="water" component={Water}></Route>
+    </Route>
+  </Router>
+  , document.getElementById('root'));
