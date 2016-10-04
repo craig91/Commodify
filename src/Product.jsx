b2eced7a6266191af255284ee57d0e3b48ea20e0
gabemb <gabrielbny@gmail.com>
2016-10-04 10:36:26 -0400

    added switch/case to Product.jsx. FOrmatted data.js for readability

--- src/Product.jsx
+++ src/Product.jsx
@@ -10,25 +10,26 @@ const Product = React.createClass({
 	componentDidMount(){
 		let category = this.props.params.category
 		let prodName = this.props.params.product
-		Switch (category){
+		let product
+		switch (category){
 			case "water":
-				let product = data.getWater().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getWater().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 				break;
 			case "air":
-				let product = data.getAir().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getAir().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 			case "seeds":
-				let product = data.getSeeds().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getSeeds().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 			case "land":
-				let product = data.getLand().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getLand().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 			case "thing":
-				let product = data.getThing().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getThing().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 			case "nextThing":
-				let product = data.getnextThing().filter(prodObj => prodName === prodObj.name ? prodObj : null);
+				product = data.getnextThing().filter(prodObj => prodName === prodObj.name ? prodObj : null);
 				this.setState({name: product.name, image: product.images, description: product.description, price: product.price});
 				break;
 		}	
