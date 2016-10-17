f4a7efee59486c1c774cfbb88f22672a34b944b9
Rabiya Watson-Maiorana <ormabe@nyc.rr.com>
2016-10-17 15:34:48 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+// React Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import { Nav, Navbar, NavItem} from 'react-bootstrap';
+
+// CSS Imports
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+// App Imports
+import NavBar from './navlarge'
+import Footer from './footer.js'
+
+// Page Imports
+import CategoryPage from './categoryPage.js'
+import Product from './Product.jsx'
+import HomePage from './HomePage'
+
+// Footer Imports
+import FAQ from './FAQ.js';
+import Contact from './Contact.js'
+import About from './About.js'
+import NonProfits from './Nonprofits.jsx'
+
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <NavBar />
+        {this.props.children}
+         <Footer />
+      </div>
+    )
+  }
+})
+
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+      <Route path='/company/faq' component={FAQ}></Route>
+      <Route path='/company/about' component={About}></Route>
+      <Route path='/company/nonprofits' component={NonProfits}></Route>
+      <Route path='/company/contact' component={Contact}></Route>
+      <Route path=":category" component={CategoryPage}></Route>
+      <Route path='/:category/:id' component={Product}></Route>
+    </Route>
+  </Router>
+,document.getElementById('root'));
---
+++ footer.js
@@ -0,0 +1,50 @@
+// React Import
+import React from 'react';
+import {Link} from 'react-router';
+
+// CSS Import
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+
+// <footer className="footer ">
+// 				<div className="panel-footer panel-custom">
+// 					<div className="container">
+// 		  				<div >
+// 			    			<div >
+// 				      			<div >
+// 					       			 <Link to='/company/about'> About Us </Link> | 
+// 					       			 <Link to='/company/contact'> Contact Us </Link> | 
+// 					       			 <Link to='/company/nonprofits'> NonProfits </Link> | 
+// 					       			 <Link to='/company/faq'> FAQ</Link> 
+// 					      		</div>
+// 					      		<div >
+// 					       		 	 <p className="muted pull-right">© 2016 COMODiFi. All rights reserved</p>
+// 					      		</div>
+// 				    		</div>
+// 			  			</div>
+// 					</div>
+// 				</div>
+// 			</footer>
+
+var Footer = React.createClass({
+	render(){
+		return (
+			<div id="footer-id" className="flex-container">
+				<footer className="footer">
+					<div className="footer-info">
+ 					      <Link to='/company/about'>About Us </Link> | 
+ 					      <Link to='/company/contact'> Contact Us </Link> | 
+ 					      <Link to='/company/nonprofits'> NonProfits </Link> | 
+ 					      <Link to='/company/faq'> FAQ</Link> 
+ 					 </div>
+ 					 <div >
+ 					      <p className="muted pull-right">© 2016 COMODiFi. All rights reserved</p>
+					 </div>
+				</footer>
+			</div>
+		)
+	}
+});
+
+export default Footer;
